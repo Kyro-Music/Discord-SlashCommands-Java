62b42bfbfb25aa5e44dc7b4a3dc44232da9580d3
BuildTools <unconfigured@null.spigotmc.org>
2021-01-21 16:49:06 +0100

    Added InteractionMessage.kt

--- src/main/java/de/Jan/SlashCommands/Interaction.kt
+++ src/main/java/de/Jan/SlashCommands/Interaction.kt
@@ -1,7 +1,5 @@
 package de.Jan.SlashCommands
 
-import net.dv8tion.jda.api.entities.MessageEmbed
-import net.dv8tion.jda.api.entities.Webhook
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -9,14 +7,18 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONArray
 import org.json.JSONObject
 
-class Interaction(private val builder: SlashCommandBuilder, private val interactionToken: String, private  val token: String, private val id: String, private val interactionID: String) {
+class Interaction(private val builder: SlashCommandBuilder, val interactionToken: String, private  val token: String, private val id: String, private val interactionID: String) {
 
     private val callback_url = "https://discord.com/api/v8/interactions/$interactionID/$interactionToken/callback"
     private val client = OkHttpClient()
     private val JSON = "application/json; charset=utf-8".toMediaType()
+    private var callback = false
 
-
-    fun callback(type: Int, value: String? = null) {
+    fun callback(type: Int, value: String? = null) : InteractionMessage? {
+        if(callback) {
+            return null
+        }
+        callback = true
         val i = JSONObject()
         i.put("type", type)
         if(value != null) {
@@ -32,10 +34,16 @@ class Interaction(private val builder: SlashCommandBuilder, private val interact
                 .addHeader("Authorization", "Bot $token")
                 .post(i.toString().toRequestBody(JSON))
         val call = client.newCall(r.build()).execute()
-        builder.checkIfError(call.body!!.string())
+        val result = call.body!!.string()
+        builder.checkIfError(result)
+        if(value != null) {
+            return InteractionMessage(value, "", builder, this, true)
+        } else {
+            return null
+        }
     }
 
-    fun sendMessage(value: String) {
+    fun sendMessage(value: String) : InteractionMessage {
         val createURL = "https://discord.com/api/v8/webhooks/${builder.botID}/$interactionToken"
             val data = JSONObject()
                     .put("tts", false)
@@ -47,7 +55,10 @@ class Interaction(private val builder: SlashCommandBuilder, private val interact
                 .addHeader("Authorization", "Bot $token")
                 .post(data.toString().toRequestBody(JSON))
         val call = client.newCall(r.build()).execute()
-        builder.checkIfError(call.body!!.string())
+        val result = call.body!!.string()
+        builder.checkIfError(result)
+        val message = JSONObject(result)
+        return InteractionMessage(message.getString("content"), message.getString("id"), builder, this, false)
     }
 
 }
---
+++ src/main/java/de/Jan/SlashCommands/InteractionMessage.kt
@@ -0,0 +1,49 @@
+package de.Jan.SlashCommands
+
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONArray
+import org.json.JSONObject
+
+class InteractionMessage(var content: String, val id: String, private val builder: SlashCommandBuilder, val interaction: Interaction, val callback: Boolean) {
+
+    private val client = OkHttpClient()
+    private val JSON = "application/json; charset=utf-8".toMediaType()
+
+    fun edit(value: String) {
+        this.content = value
+        var createURL = "https://discord.com/api/v8/webhooks/${builder.botID}/${interaction.interactionToken}/messages/$id"
+        if(callback) {
+            createURL = "https://discord.com/api/v8/webhooks/${builder.botID}/${interaction.interactionToken}/messages/@original"
+        }
+        val data = JSONObject()
+                .put("tts", false)
+                .put("content", value)
+                .put("embeds", JSONArray())
+                .put("allowed_mentions", JSONArray())
+        val r = Request.Builder()
+                .url(createURL)
+                .addHeader("Authorization", "Bot ${builder.token}")
+                .patch(data.toString().toRequestBody(JSON))
+        val call = client.newCall(r.build()).execute()
+        val result = call.body!!.string()
+        builder.checkIfError(result)
+    }
+
+    fun delete() {
+        var createURL = "https://discord.com/api/v8/webhooks/${builder.botID}/${interaction.interactionToken}/messages/$id"
+        if(callback) {
+            createURL = "https://discord.com/api/v8/webhooks/${builder.botID}/${interaction.interactionToken}/messages/@original"
+        }
+        val r = Request.Builder()
+                .url(createURL)
+                .addHeader("Authorization", "Bot ${builder.token}")
+                .delete()
+        val call = client.newCall(r.build()).execute()
+        val result = call.body!!.string()
+        builder.checkIfError(result)
+    }
+
+}
